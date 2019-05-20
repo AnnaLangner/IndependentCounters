84aac297bd47ffe8ed764f597c7ea23df8ba9ed5
Ania <annalangner06@gmail.com>
2019-05-20 13:56:05 +0200

    Adding a component that increases the counter

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var Counter = React.createClass({
+	getTnstalState: function () {
+		return {
+			counter: 0
+		};
+	},
+
+	increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+	
+	render: function () {
+		return React.createElement('div', {},
+			React.createElement('span', {}, 'Counter' + this.state.counter)
+		);
+	}
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(elemen, document.getElementById('app'));
